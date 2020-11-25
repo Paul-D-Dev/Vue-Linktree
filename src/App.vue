b4f9bfd8c54d3ea05899a751e56e0e12670586cb
Paul DAO <dao.van.paul@gmail.com>
2020-11-25 17:17:13 +0100

    FEAT Link component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,76 @@
 <template>
-  <img alt="DAO logo" src="./assets/logo_dao.png">
-  <SocialNetwork/>
+
+  <main>
+    <div class="img-wrapper">
+      <img alt="DAO logo" src="./assets/logo_dao.png" />
+    </div>
+    <Link />
+    <SocialNetwork class="social"/>
+  </main>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
 import SocialNetwork from './components/SocialNetwork.vue';
+import Link from './components/Link.vue';
 
 @Options({
   components: {
-    HelloWorld,
-    SocialNetwork
+    SocialNetwork,
+    Link
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
+body {
+  background-color: #3D3B3C
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+main {
+  height: 100vh;
+  max-width: 680px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 20px;
+  padding-right: 20px;
+  display: flex;
+  flex-direction: column;
+
+
+  .social {
+    margin-top: auto;
+    margin-bottom: 60px;
+  }
+}
+
+a {
+  text-decoration: none;
+  color: unset;
+}
+
+.img-wrapper {
+  width: 100%;
+  height: 200px;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+
+
 </style>
--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,13 +1,57 @@
 <template>
+
+    <ul class="link__btn-wrapper">
+        <li class="link__btn"
+            v-for="link in links" 
+            :key="link.id">
+                <a :href="link.url">{{link.name}}</a>
+        </li>
+    </ul>
   
 </template>
 
 <script>
+import {Vue} from 'vue-class-component';
+
 export default class Link extends Vue {
-    Links
+    links = [
+        { id: 1, name:'Notre site', url:"https://daodavy.com"},
+        { id: 2, name:'Mon super site', url:"https://daodavy.com" }
+        ];
 }
 </script>
 
-<style>
+<style lang="scss">
 
+$color : salmon;
+$bg-color: #3D3B3C;
+
+.link__btn {
+    display: flex;
+    width: 100%;
+    min-height: 60px;
+    background-color: $color;
+    border:2px solid $color;
+    transition: all 0.1s ease-in-out;
+    overflow: hidden;
+    
+    &:not(:first-of-type) {
+        margin-top: 20px;
+    }
+    &:hover {
+        background-color: transparent;
+        a {
+            color : $color;
+        }
+    }
+
+    a {
+        width: 100%;
+        padding: 10px;
+        display: flex;
+        justify-content: center;
+        align-self: center;
+        color: $bg-color;
+    }
+}
 </style>
